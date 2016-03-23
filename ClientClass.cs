9991b515df99193b4ea96bb26d973b6034224639
WioZak <wiolet.00@gmail.com>
2016-03-23 20:31:14 +0100

    first commit

---
+++ ClientClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace DesktopApp
+{
+    class ClientClass
+    {
+        public static void Connect(string ip, int port, string command) //get command from button
+        {
+
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                tcpclnt.Connect(ip, port);
+
+                //Enter the string to be transmitted:
+                String str = command;
+                Stream stm = tcpclnt.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                //Transmitting.....
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Text.RegularExpressions;
+
+namespace DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string IP { get; set; }
+        public static int Port { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            goButton.Click += GoButton_Click;
+            stopButton.Click += StopButton_Click;
+        }
+
+        private void ipTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox ipTextBox = (TextBox)sender;
+            IP = ipTextBox.Text;
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void portTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox portTextBox = (TextBox)sender;
+            Port = Convert.ToInt32(portTextBox.Text);
+        }
+
+        private void GoButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClientClass.Connect(IP, Port, "g");
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClientClass.Connect(IP, Port, "s");
+        }
+    }
+}
